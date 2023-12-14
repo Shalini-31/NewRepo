4068e8a70e81bdc76f5d72af17fcdc0910f05015
shalini.ss <ss4503@srmist.edu.in>
2023-12-14 14:58:09 +0530

    static constructor

--- ProdaptCSharp.Day5/Program.cs
+++ ProdaptCSharp.Day5/Program.cs
@@ -4,18 +4,43 @@
 {
     public class Program
     {
-        static void Main(string[] args)
+         static int count = 10;
+        int stdid;
+
+        public Program()
         {
-            string name = "Shalini";
-            string description = " hi its me hello";
-            //sub string 
-            Console.WriteLine(name.Substring(1,6));
-            //to upper to lower 
-            Console.WriteLine(name.ToLower());
-            Console.WriteLine(name.ToUpper());
-            //trim() contain()
-            Console.WriteLine(description.Contains("hello"));//return bool
-            Console.WriteLine(description.TrimStart());//remove space at beginning and end not middle.
+            count++;
+            stdid = count;
+        }
+
+        static Program()
+        {
+            count = 100;
+        }
+
+        
+        public static void Main(string[] args)
+        {
+            //    string name = "Shalini";
+            //    string description = " hi its me hello";
+            //    //sub string 
+            //    Console.WriteLine(name.Substring(1,6));
+            //    //to upper to lower 
+            //    Console.WriteLine(name.ToLower());
+            //    Console.WriteLine(name.ToUpper());
+            //    //trim() contain()
+            //    Console.WriteLine(description.Contains("hello"));//return bool
+            //    Console.WriteLine(description.TrimStart());//remove space at beginning and end not middle.
+
+            //    x = 20;
+            //    Console.WriteLine(x);//Static example 
+
+            //    //Rename Ctrl+R Ctrl+R
+
+            Program obj=new Program();
+            Program obj2 = new Program();
+            Console.WriteLine($" static constructor called {obj.stdid}");
+            Console.WriteLine($"instance of the class{obj2.stdid}");
         }
     }
 }
