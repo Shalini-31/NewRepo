cb34c0fba5fa5fb5b72744ea510d47a06e6f088e
shalini.ss <shalini.ss@CLAP1024>
2023-12-14 10:36:52 +0530

    Add project files.

---
+++ Prodapt.CSharp.Day4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Prodapt.CSharp.Library;
+
+
+namespace Prodapt.CSharp.Day4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Login obj1 = new Login();
+            var result=obj1.Validation("admin","admin@123");
+            Console.WriteLine(result ? "Valid User" : "Invalid User");
+                }
+    }
+}
---
+++ ProdaptCSharp.Day5/Program.cs
@@ -0,0 +1,13 @@
+﻿using System; 
+    namespace ProdaptCSharp.Day5
+    
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime today = DateTime.Today;
+
+        }
+    }
+}
